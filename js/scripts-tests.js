6520b35b7b69679afe6e0f67684ac136b1d5ca97
Randel M <Randel.C.Moore@gmail.com>
2020-11-06 10:58:32 -0800

    add variables to define prices

--- js/scripts-tests.js
+++ js/scripts-tests.js
@@ -20,4 +20,19 @@ PizzaOrder.prototype.AddPizza = function(pizza) {
 
 let pizza1 = new Pizza("small", "sausage", "onion");
 pizzaOrder.AddPizza(pizza1);
-console.log(pizzaOrder.pizzas)
+console.log(pizzaOrder.pizzas);
+
+//Size
+const small = ["Small", 10];
+const medium = ["Medium", 15];
+const large = ["Large", 20];
+
+//Meat
+const sausage = ["Sausage", 1];
+const chicken = ["Chicken", 1];
+const deer = ["Deer", 3];
+
+//Vegi
+const onion = ["Onion", .5]
+const carrots = ["Carrots", .5]
+const turnips = ["Turnips", 1]
--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,16 @@ function Pizza(size,meat,vegi) {
     this.meat = meat;
     this.vegi = vegi;
   }
-console.log(pizza)
+console.log(pizza);
+
+PizzaOrder.prototype.AddPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+let pizza1 = new Pizza("small", "sausage", "onion");
+pizzaOrder.AddPizza(pizza1);
+console.log(pizzaOrder.pizzas)
+
 
 
 
