ee1ac0a0c5f42a36f7958dd022d2b14f659e65de
Randel M <Randel.C.Moore@gmail.com>
2020-11-06 10:20:14 -0800

    add code to pass spec 1 for Pizza()

--- js/scripts-tests.js
+++ js/scripts-tests.js
@@ -1,13 +1,15 @@
-let pizzaOrder = new PizzaOrder()
+let pizzaOrder = new PizzaOrder();
 
 function PizzaOrder(pizzas) {
-  this.pizzas = []
+  this.pizzas = [];
 }
 console.log(pizzaOrder);
   
+let pizza = new Pizza();
 
-// function Pizzas(size,meat,vegi) {
-  //   this.size = size;
-  //   this.meat = meat;
-  //   this.vegi = vegi;
-  // }
+function Pizza(size,meat,vegi) {
+    this.size = size;
+    this.meat = meat;
+    this.vegi = vegi;
+  }
+console.log(pizza)
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 //Business Logic
-let pizzaOrder = new PizzaOrder()
+let pizzaOrder = new PizzaOrder();
 
-function PizzaOrder(size,meat,vegi) {
-  this.size = size;
-  this.meat = meat;
-  this.vegi = vegi;
+function PizzaOrder(pizzas) {
+  this.pizzas = [];
 }
+console.log(pizzaOrder);
+  
+let pizza = new Pizza();
+
+function Pizza(size,meat,vegi) {
+    this.size = size;
+    this.meat = meat;
+    this.vegi = vegi;
+  }
+console.log(pizza)
+
 
 
 
