17d8bda8a06bd16ce530496daf00aeef067a35cc
Randel M <Randel.C.Moore@gmail.com>
2020-11-06 12:12:13 -0800

    add code to pass spec 2 of PizzaOrder.prototype.PizzaCalcPrice = function()

--- js/scripts-tests.js
+++ js/scripts-tests.js
@@ -36,12 +36,18 @@ PizzaOrder.prototype.AddPizza = function(pizza) {
 
 
 PizzaOrder.prototype.PizzaCalcPrice = function() {
-  let pizzaPrice = this.pizzas[0].size[1] + this.pizzas[0].meat[1] + this.pizzas[0].vegi[1];
-  return pizzaPrice;
+  let pizzaPriceArray = [];
+  for (let i = 0; i < this.pizzas.length; i++) {
+  pizzaPrice = this.pizzas[i].size[1] + this.pizzas[i].meat[1] + this.pizzas[i].vegi[1];
+  pizzaPriceArray.push(pizzaPrice)
+  }
+  return pizzaPriceArray;
 }
 
 let pizza1 = new Pizza(small, sausage, onion);
 pizzaOrder.AddPizza(pizza1);
-console.log(pizzaOrder.pizzas);
 
-pizzaOrder.PizzaCalcPrice()
+let pizza2 = new Pizza(large, deer, none);
+pizzaOrder.AddPizza(pizza2);
+
+//pizzaOrder.PizzaCalcPrice()
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-//Business Logic
-
 //General Pricing
 const small = ["Small", 10];
 const medium = ["Medium", 15];
@@ -38,15 +36,21 @@ PizzaOrder.prototype.AddPizza = function(pizza) {
 
 
 PizzaOrder.prototype.PizzaCalcPrice = function() {
-  let pizzaPrice = this.pizzas[0].size[1] + this.pizzas[0].meat[1] + this.pizzas[0].vegi[1];
-  return pizzaPrice;
+  let pizzaPriceArray = [];
+  for (let i = 0; i < this.pizzas.length; i++) {
+  pizzaPrice = this.pizzas[i].size[1] + this.pizzas[i].meat[1] + this.pizzas[i].vegi[1];
+  pizzaPriceArray.push(pizzaPrice)
+  }
+  return pizzaPriceArray;
 }
 
 let pizza1 = new Pizza(small, sausage, onion);
 pizzaOrder.AddPizza(pizza1);
-console.log(pizzaOrder.pizzas);
 
-pizzaOrder.PizzaCalcPrice()
+let pizza2 = new Pizza(large, deer, none);
+pizzaOrder.AddPizza(pizza2);
+
+//pizzaOrder.PizzaCalcPrice()
 
 
 
