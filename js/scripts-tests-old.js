1213ce0184ac1190f962d8271de1487bcc3c3733
Randel M <Randel.C.Moore@gmail.com>
2020-11-09 07:37:52 -0800

    rename 'checkbox' files to standard index and scropts convention

---
+++ js/scripts-tests-old.js
@@ -0,0 +1,77 @@
+//General Pricing
+const small = ["Small", 10];
+const medium = ["Medium", 15];
+const large = ["Large", 20];
+const none = ["none", 0];
+
+//Meat
+const sausage = ["Sausage", 1];
+const chicken = ["Chicken", 1];
+const deer = ["Deer", 3];
+
+//Vegi
+const onion = ["Onion", .5];
+const carrots = ["Carrots", .5];
+const turnips = ["Turnips", 1];
+
+let pizzaOrder = new PizzaOrder();
+// let pizza = new Pizza();
+
+function PizzaOrder() {
+  this.pizzas = [];
+}
+  
+function Pizza(size,meat,vegi) {
+    this.size = size;
+    this.meat = meat;
+    this.vegi = vegi;
+  }
+
+PizzaOrder.prototype.AddPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+// PizzaOrder.prototype.PizzaCalcPrice = function() {
+//   let pizzaPriceArray = [];
+//   for (let i = 0; i < this.pizzas.length; i++) {
+//   pizzaPrice = this.pizzas[i].size[1] + this.pizzas[i].meat[1] + this.pizzas[i].vegi[1];
+//   pizzaPriceArray.push(pizzaPrice);
+//   }
+//   return pizzaPriceArray;
+// }
+
+PizzaOrder.prototype.OrderCalcPrice = function() {
+  pizzaPrice = 0;
+  for (let i = 0; i < this.pizzas.length; i++) {
+  pizzaPrice += this.pizzas[i].size[1] + this.pizzas[i].meat[1] + this.pizzas[i].vegi[1];
+  }
+  return pizzaPrice;
+}
+
+PizzaOrder.prototype.LastPizzaPrice = function() {
+  lastPizzaPrice = this.pizzas[this.pizzas.length - 1].size[1] + this.pizzas[this.pizzas.length - 1].meat[1] + this.pizzas[this.pizzas.length - 1].vegi[1];
+  return lastPizzaPrice;
+}
+
+// PizzaOrder.prototype.Toppings = function() {
+//   for (let i = 0; i < this.pizza; i++) {
+
+//   }
+// }
+
+//User Interface Logic
+$(document).ready(function() {
+  $("#formSubmit").click(function(event) {
+    event.preventDefault();
+    let pizzaSize = eval($("input:radio[name=pizzaSize]:checked").val());
+    let meatType = eval($("input:radio[name=meatType]:checked").val());
+    let vegiType = eval($("input:radio[name=vegiType]:checked").val());
+    pizza = new Pizza(pizzaSize, meatType, vegiType);
+    pizzaOrder.AddPizza(pizza);
+    $("#pizzaList").append("<li>" + pizzaOrder.LastPizzaPrice() + "</li>");
+    $("#outputOrderPrice").text(pizzaOrder.OrderCalcPrice());
+    console.log();
+  });
+});
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,103 @@
+//General Pricing
 const small = ["Small", 10];
 const medium = ["Medium", 15];
 const large = ["Large", 20];
-const none = ["none", 0];
 
-const sausage = ["Sausage", 1];
-const chicken = ["Chicken", 1];
-const deer = ["Deer", 3];
+//Cheese
+const gargonzola = ["Gargonzola", 1];
+const stilton = ["Stilton", 1.5];
+const limburger = ["Limburger", 2];
+const camelCheese = ["Camel Cheese", 3];
+const casuMartzu = ["Casu Martzu", 4.5];
 
-const onion = ["Onion", .5];
-const carrots = ["Carrots", .5];
-const turnips = ["Turnips", 1];
+//Meats
+const armadillo = ["Armadillo", 1];
+const deer = ["Deer", 4.5];
+const opossom = ["Opossom", .5];
+const alligator = ["Alligator", 2];
+const kangaroo = ["Kangaroo", 3];
 
+//Veggies
+const rutabaga = ["Rutabaga", 1];
+const blueGreenAlgae = ["Blue-Green Algae", .5];
+const grass = ["Grass", .5];
+const palmFronds = ["Palm Fronds", 2];
+const cactus = ["Cactus", 2];
+
+//Extras
+const roastedAnts = ["Roasted Ants", 1];
+const hornToadTears = ["Horn-Toad Tears", 6];
+const tarantulaHair = ["Tarantula Hair", 2.5];
+const gratedLichen = ["Grated Lichen", 2];
+const alkaSeltzer = ["Alka-Seltzer", 20];
+
+let orderPrice = 0;
 let pizzaOrder = new PizzaOrder();
 
 function PizzaOrder() {
   this.pizzas = [];
 }
   
-function Pizza(size,meat,vegi) {
+function Pizza(size, toppings) {
     this.size = size;
-    this.meat = meat;
-    this.vegi = vegi;
+    this.toppings = toppings;
   }
+
+function Toppings(name, price) {
+    this.name = name;
+    this.price = price;
+};
 
 PizzaOrder.prototype.AddPizza = function(pizza) {
   this.pizzas.push(pizza);
 }
 
-PizzaOrder.prototype.OrderCalcPrice = function() {
-  pizzaPrice = 0;
-  for (let i = 0; i < this.pizzas.length; i++) {
-  pizzaPrice += this.pizzas[i].size[1] + this.pizzas[i].meat[1] + this.pizzas[i].vegi[1];
+PizzaOrder.prototype.LastPizzaPrice = function() {
+  let lastPizzaPrice = 0;
+  for (let i = 0; i < this.pizzas[this.pizzas.length - 1].toppings.length; i++) {
+    lastPizzaPrice += (this.pizzas[this.pizzas.length - 1].toppings[i].price);
   }
-  return pizzaPrice;
+  return lastPizzaPrice + this.pizzas[this.pizzas.length - 1].size[1];
 }
 
-PizzaOrder.prototype.LastPizzaPrice = function() {
-  lastPizzaPrice = this.pizzas[this.pizzas.length - 1].size[1] + this.pizzas[this.pizzas.length - 1].meat[1] + this.pizzas[this.pizzas.length - 1].vegi[1];
-  return lastPizzaPrice;
+PizzaOrder.prototype.OrderCalcPrice = function() {
+  orderPrice += this.LastPizzaPrice();
+  return orderPrice;
+}
+
+PizzaOrder.prototype.Size = function() {
+  let sizeListArray = [];
+  for (let i = 1; i < this.pizzas[this.pizzas.length - 1].size.length; i++) {
+    sizeListArray.push(this.pizzas[this.pizzas.length - 1].size[0]);
+    console.log(sizeListArray);
+  }
+  return sizeListArray.join("");
+}
+
+PizzaOrder.prototype.Toppings = function() {
+  let toppingsListArray = [];
+  for (let i = 0; i < this.pizzas[this.pizzas.length - 1].toppings.length; i++) {
+    toppingsListArray.push(this.pizzas[this.pizzas.length - 1].toppings[i].name);
+  }
+  return toppingsListArray.join(", ");
 }
 
 
 $(document).ready(function() {
   $("#formSubmit").click(function(event) {
     event.preventDefault();
+    let toppingsArray = [];
     let pizzaSize = eval($("input:radio[name=pizzaSize]:checked").val());
-    let meatType = eval($("input:radio[name=meatType]:checked").val());
-    let vegiType = eval($("input:radio[name=vegiType]:checked").val());
-    pizza = new Pizza(pizzaSize, meatType, vegiType);
+      $("input:checkbox[name=toppings]:checked").each(function() {
+      let toppings = new Toppings(eval($(this).val())[0], eval($(this).val())[1]);
+      toppingsArray.push(toppings);
+    });
+    let pizza = new Pizza(pizzaSize, toppingsArray);
     pizzaOrder.AddPizza(pizza);
-    $("#pizzaList").append("<li>" + pizzaOrder.LastPizzaPrice() + "</li>");
+    $("#pizzaList").append("<li>" + pizzaOrder.LastPizzaPrice() + "<ul>" + pizzaOrder.Size() + ": " + "<span style='font-size: 12pt'>" + pizzaOrder.Toppings() + "</span>" + "</ul>" + "</li>");
     $("#outputOrderPrice").text(pizzaOrder.OrderCalcPrice());
+  });
+  $("#orderButton").click(function(event) {
+    alert("Just kidding! This isn't real website.");
   });
 });
